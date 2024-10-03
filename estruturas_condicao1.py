e2e9b0afbebc229ad5c1c06bf61985bffe578329
joaogabrielvalenca <joaogabrielvalenca@gmail.com>
2024-10-03 09:10:49 -0300

    primeira entrega

---
+++ estruturas_condicao1.py
@@ -0,0 +1,11 @@
+temperatura = 31
+
+
+def checar_temperatura(temperatura):
+    if temperatura < 30:
+        print('A temperatura hoje está amena')
+    else:
+        print('Hoje está fazendo calor')
+
+
+checar_temperatura(temperatura)
